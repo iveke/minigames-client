c855b4948b80dc84219a425b108f1e3f6cae0132
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2025-04-21 23:21:48 +0300

    added filed error note component

---
+++ components/fields/field-error.vue
@@ -0,0 +1,56 @@
+<script setup>
+const props = defineProps({
+  message: {
+    default: false,
+    required: false,
+  },
+  type: {
+    type: String,
+    default: 'error' | 'warn' | 'success',
+    required: true,
+  }
+})
+console.log(props.message)
+</script>
+
+<template>
+  <span v-if="message"
+      :class="{
+    'field__status-note': true,
+    'error': type === 'error',
+    'warn': type === 'warn',
+    'success': type === 'success',
+  } ">
+    <Icon name="material-symbols:error" size="1rem"/>
+    {{ message }}
+  </span>
+
+</template>
+
+<style scoped>
+.field__status-note {
+  width: 100%;
+  margin-top: 0.25rem;
+  display: inline-flex;
+  gap: 0.25rem;
+  margin-bottom: -0.25rem;
+
+  &.error {
+    color: var(--error);
+  }
+
+  &.warn {
+    color: var(--warn);
+  }
+
+  &.success {
+    color: var(--success);
+  }
+
+  & .iconify {
+    margin-top: -0.25rem;
+    transform: translate(0, 33%);
+  }
+
+}
+</style>
