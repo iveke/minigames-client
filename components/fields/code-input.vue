f36c6d1f773c3422f4f5972b691d0761b81cb811
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2025-04-21 17:17:34 +0300

    Made CodeInput component working

--- components/fields/code-input.vue
+++ components/fields/code-input.vue
@@ -2,117 +2,79 @@
 const props = defineProps({
   codeMask: {
     type: String,
-    default: '00000'
+    default: '10000-99999'
   },
 })
+const emit = defineEmits(['update:modelValue'])
+
+const codeMinValue = Number(props.codeMask.split('-')[0])
+const codeMaxValue = Number(props.codeMask.split('-')[1])
+const codeLength = (props.codeMask.length - 1) / 2
 
 
-const codeLength = props.codeMask.length;
-const codeMaxValue = Number(props.codeMask.replace(/0/g, '9'));
-const codeMinValue = Number(codeMaxValue / 9) - (codeMaxValue / 9) % (10 ** (codeLength - 1));
+const inputRefs = ref([])
+const inputVModels = ref(Array.from({length: codeLength}, () => ''))
 
-
-const inputCode = ref(null);
-const cursorPosition = computed(() => {
-  return inputCode.value ? inputCode.value.toString().length : 0
-});
-const inputFieldID = computed(() => {
-  return cursorPosition.value > 0 ? cursorPosition.value : 1
-});
-
-const currentInput = computed(() => {
-  return document.querySelector(`#code__${inputFieldID.value}`)
-});
-const definiteInput = (id) => {
-  return document.querySelector(`#code__${id}`)
-}
-
-
-function WriteDigit(number) {
-  let newCodeValue = inputCode.value * 10 + number
-
-  if (newCodeValue <= codeMaxValue) {
-    inputCode.value *= 10;
-    inputCode.value += number;
-
-    currentInput.value.value = number
-  }
-}
-
-function EraseDigit() {
-  let newCodeValue = inputCode.value / 10 - inputCode.value / 10 % 1
-  if (newCodeValue > 0) {
-    inputCode.value /= 10;
-    inputCode.value -= inputCode.value % 1;
-
-    currentInput.value.nextSibling.value = ''
+watchEffect(() => {
+  const joinedCode = inputVModels.value.join('')
+  if (isValid(joinedCode)) {
+    emit('update:modelValue', Number(joinedCode))
   } else {
-    inputCode.value = null;
-    currentInput.value.value = ''
+    emit('update:modelValue', null)
 
   }
+})
+const code = computed(() => inputVModels.value.join(''))
+
+function isValid(code) {
+  return code.length === codeLength && Number(code) >= codeMinValue && Number(code) <= codeMaxValue
 }
 
-
-function handleInput(event) {
-  const CodeNumber = Number(event.key)
-  const input = event.target;
-
-  if (isNaN(CodeNumber)) {
-    if (event.key === 'Backspace') {
-      EraseDigit()
-      moveFocus(-1)
-    }
-  } else {
-    event.target.value = ''
-    WriteDigit(CodeNumber)
-    moveFocus(1)
-  }
-}
-
-
-function moveFocus(indexOffset) {
-  if (!currentInput.value) {
-    console.error('No current input found')
-    return
-  }
-
-
-  if (indexOffset === 0) {
-    if (cursorPosition.value === 0 || cursorPosition.value === codeLength) {
-      currentInput.value.focus()
-    } else {
-      const nextInput = currentInput.value.nextElementSibling
-      if (nextInput) {
-        nextInput.focus()
-      }
-    }
-  } else if (indexOffset === -1) {
-    if (cursorPosition.value === 0) {
-      currentInput.value.focus()
-    } else if (cursorPosition.value === 4) {
-      definiteInput(5).focus()
-    } else {
-      const nextInput = currentInput.value.nextElementSibling
-      if (nextInput) {
-        nextInput.focus()
-      }
-    }
-  } else if (indexOffset === 1) {
-    if (cursorPosition.value >= 0 && cursorPosition.value < codeLength) {
-      const nextInput = currentInput.value.nextElementSibling
-      if (nextInput) {
-        nextInput.focus()
-      }
-    }
-  }
-
-
-}
 
 function handlePaste(event) {
-  console.log('paste', event.clipboardData.getData('text'))
+  const pastedCode = event.clipboardData.getData('text')
+  if (isValid(pastedCode)) {
+    inputVModels.value = pastedCode.split('').slice(0, codeLength)
+  } else {
+  }
+}
 
+function handleInput(index, event) {
+  const input = event.target
+  if (isNaN(Number(input.value))) {
+    inputVModels.value[index] = ''
+  } else {
+    focusLastEmpty(true)
+  }
+}
+
+function handleBackspace(index, event) {
+  if (event.key === 'Backspace') {
+    if (code.value.length === 0) {
+      inputRefs.value[0].blur()
+    } else if (code.value.length === codeLength) {
+      inputVModels.value[codeLength - 1] = ''
+    } else if (index > 0 && inputVModels.value[index] === '') {
+      inputVModels.value[index - 1] = ''
+      focusLastEmpty()
+    }
+  }
+}
+
+function focusLastEmpty(blur = false) {
+  if (code.value.length === codeLength) {
+    inputRefs.value[codeLength - 1].focus()
+    if (blur) {
+      inputRefs.value[codeLength - 1].blur()
+    }
+  } else {
+    for (let i = 0; i < inputVModels.value.length; i++) {
+      if (inputVModels.value[i] === '') {
+        inputRefs.value[i].focus()
+        break
+      }
+    }
+  }
 }
 
 </script>
@@ -121,9 +83,10 @@ function handlePaste(event) {
   <div class="code-field">
 
     <input v-for="i in codeLength"
-
            :key="i"
            :id="'code__' + i"
+           :ref="el => inputRefs[i-1] = el"
+           v-model="inputVModels[i-1]"
 
            type="text"
            inputmode="numeric"
@@ -133,14 +96,13 @@ function handlePaste(event) {
            class="code-field__input"
            placeholder="_"
            @paste="handlePaste"
-           @keydown.prevent="handleInput"
-           @mousedown.prevent="moveFocus(0)"
+           @input="handleInput(i-1, $event)"
+           @keydown="handleBackspace(i-1, $event)"
+           @focus="focusLastEmpty()"
     >
+    <!--    {{ code }}-->
   </div>
 
-  {{ inputCode }}<br>
-  Cursor: {{ cursorPosition }}<br>
-  Field: {{ inputFieldID }}<br>
 </template>
 
 <style scoped>
