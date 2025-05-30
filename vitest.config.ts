75a598eaf4352046983dc4184bab262ac21af94b
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2025-05-31 01:19:20 +0300

    fixtest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'vitest/config';
 import path from 'path';
 
 export default defineConfig({
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './tests/setup.js',
-    },
     resolve: {
         alias: {
             '~': path.resolve(__dirname),
