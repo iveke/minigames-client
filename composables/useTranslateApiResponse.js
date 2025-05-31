946a289b38f32d7c983efcabc3c0cb3e4ce2624b
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2025-05-31 10:52:39 +0300

    fix defalut error message

--- composables/useTranslateApiResponse.js
+++ composables/useTranslateApiResponse.js
@@ -1,16 +1,17 @@
 export const useTranslateApiResponse = () => {
-    const { t, te } = useI18n();
+  const {t, te} = useI18n();
 
-    function translate(response, endpoint) {
-        const translationKey = `statusMessages.error.${endpoint}.${response.status}`
-        const translation = te(translationKey) ? t(translationKey) : (() => {
-            console.error('Missing key', translationKey)
-            // return t('apiStatusText.default')
-            return response.statusText
-        })()
-        return translation
-    }
-    return {
-        translate
-    }
+  function translate(response, endpoint) {
+    const translationKey = `statusMessages.error.${endpoint}.${response.status}`
+    const translation = te(translationKey) ? t(translationKey) : (() => {
+      console.error('Missing key', translationKey)
+      // return t('apiStatusText.default')
+      return t('statusMessages.error.default')
+    })()
+    return translation
+  }
+
+  return {
+    translate
+  }
 }
